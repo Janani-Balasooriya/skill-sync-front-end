5e6f28207a3c61f5fa90c5e6f6f6feb3df3208e1
Janani-Balasooriya <jananidev7@gmail.com>
2024-11-05 22:11:42 +0530

    Display end part of url using useParams hook job page

--- src/pages/job/job.page.jsx
+++ src/pages/job/job.page.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+
 function JobPage(){
+
+    const params = useParams();
+
     return (
         <main className="p-4">
             <h1>Job Page</h1>
+            <p>{params.id}</p>
         </main>
     );
 }
