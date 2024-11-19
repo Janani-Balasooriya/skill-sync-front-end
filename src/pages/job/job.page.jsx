32b910d4a6901253d5d38146760c0cb4b09eb9a1
Janani-Balasooriya <jananidev7@gmail.com>
2024-11-19 22:38:46 +0530

    Just playing with the param

--- src/pages/job/job.page.jsx
+++ src/pages/job/job.page.jsx
@@ -4,6 +4,14 @@ function JobPage(){
 
     const params = useParams();
 
+    if(useParams().id === "abc"){
+        return (
+            <main className="p-4">
+                <h1>Job Page with ABC</h1>
+                <p>{params.id}</p>
+            </main>
+        );
+    }
     return (
         <main className="p-4">
             <h1>Job Page</h1>
